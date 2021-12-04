b1ed0645c6d1f0ea6536b92cb034e377d31b0ee3
Ilikeeatpie <starmotorcool@gmail.com>
2021-12-04 10:37:43 -0600

    i wanna see the discord user agent

--- pies-cc-api/src/app.controller.ts
+++ pies-cc-api/src/app.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 
 @Controller()
 export class AppController {
 
   @Get()
-  getHello(): string {
+  getHello(@Req() req): string {
+    console.log(req.headers["User-Agent"])
     return "<h1>Hello from pies-cc!</h1>"
   }
 }
