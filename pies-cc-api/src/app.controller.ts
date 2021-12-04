e1582cfffffc6250f16345fa6f71092a00d65c26
Ilikeeatpie <starmotorcool@gmail.com>
2021-12-04 11:28:18 -0600

    test discord embedding

--- pies-cc-api/src/app.controller.ts
+++ pies-cc-api/src/app.controller.ts
@@ -6,7 +6,11 @@ export class AppController {
   @Get()
   getHello(@Req() req, @Res() res){
     if (req.headers["user-agent"] && req.headers["user-agent"].toLowerCase().indexOf("discord") !== -1) {
-
+      return res.send(`
+      <title>pies.cc</title>
+      <meta content="pies.cc" property="og:title" />
+      <meta content="#2F3136" data-react-helmet="true" name="theme-color" />
+      `)
     }
     return res.end("<h1>Hello from pies-cc!</h1>")
   }
