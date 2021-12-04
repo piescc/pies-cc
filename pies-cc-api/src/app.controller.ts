a477df155b279f1eb7826a53663bcef39a0172b6
Ilikeeatpie <starmotorcool@gmail.com>
2021-12-04 11:06:54 -0600

    oh god

--- pies-cc-api/src/app.controller.ts
+++ pies-cc-api/src/app.controller.ts
@@ -5,7 +5,7 @@ export class AppController {
 
   @Get()
   getHello(@Req() req, @Res() res){
-    if (req.headers["user-agent"].toLowerCase().contains("discord")) {
+    if (req.headers["user-agent"] && req.headers["user-agent"].toLowerCase().contains("discord")) {
 
     }
     return res.end("<h1>Hello from pies-cc!</h1>")
