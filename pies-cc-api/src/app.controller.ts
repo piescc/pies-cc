ec43bddcf0f086505e5d9c9565578f16dd5854fb
Ilikeeatpie <starmotorcool@gmail.com>
2021-12-04 16:51:48 -0600

    test discord proxying

--- pies-cc-api/src/app.controller.ts
+++ pies-cc-api/src/app.controller.ts
@@ -7,8 +7,7 @@ export class AppController {
   getHello(@Req() req, @Res() res){
     if (req.headers["user-agent"] && req.headers["user-agent"].toLowerCase().indexOf("discord") !== -1) {
       return res.send(`
-      <title>pies.cc</title>
-      <meta content="pies.cc" property="og:title" />
+      <title>pies.cc ${req.url}</title>
       <meta content="#2F3136" data-react-helmet="true" name="theme-color" />
       `)
     }
